7f24e7dddf43b0acc429893ebcbfb15a72ad7792
Uriel Ortiz <urielortizlerma@gmail.com>
2022-08-04 20:33:36 -0600

    Se agrego el Script de la plantilla para optimizar codigo 2

--- js/tarjetaPlantilla.js
+++ js/tarjetaPlantilla.js
@@ -1,6 +1,6 @@
 const crearTarjeta = (id,imagen,nombre, artistas) =>{
     return `<div onclick="ejemplo('${id}')" data-id="${id}" class="albumSelecionado"  class="col-md-3" >
-    <div class="card h-100">
+    <div class="card h-100 ">
       <img height="400" src="${imagen}" class="card-img-top" alt="...">
       <div class="card-body">
         <h3 class="card-title text-center">${nombre}</h3>
